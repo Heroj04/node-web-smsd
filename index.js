d5405144dbb9acd9da288bb888dbc1aaf3675363
Jack Footner <jackrfootner@gmail.com>
2023-01-20 11:46:35 +1030

    Switch to use config package

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const log = require('loglevel');
 const prefix = require('loglevel-plugin-prefix');
-const args = require('args');
-
-// Set up any program arguments
-args.options([
-  {
-    name: 'logLevel',
-    description: 'Sets the logging level for the current process, overriding the config file.',
-  },
-]);
-const flags = args.parse(process.argv);
+const config = require('config');
 
 // Set up the logger
-// NOTE - Config Module will log at info level unless logLevel flag is specified
 prefix.reg(log);
 prefix.apply(log, { template: '[%t] %l (%n):' });
-log.setLevel(flags.logLevel ?? log.levels.ERROR);
-
-// Get our config
-const config = require('./lib/config');
-
-// Update the logger to use the configured logLevel
-log.getLogger('config').setLevel(flags.logLevel ?? config.logLevel);
-log.setLevel(flags.logLevel ?? config.logLevel);
+log.setLevel(config.get('logLevel'));
 
 // Load the web server
-const server = require('./lib/server');
+require('./lib/server');
 
 log.debug('Module Loaded');
